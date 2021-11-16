cfe668f7233e69aa5ca016d9a6306ce355a18e15
Lafa777 <apalllon11@gmail.com>
2021-11-16 10:32:18 +0300

    initian commit

--- src/js/main.js
+++ src/js/main.js
@@ -12,3 +12,29 @@ button.onclick = function() {
 
 }
 
+//task 1
+console.log('Алексей');
+//task 2
+console.log(09);
+//task 3
+console.log('Добро' + 'пожаловать' + 'на курс');
+//task 4
+// alert(2019);
+//task 5
+// alert(2019-200);
+//task 6
+document.getElementById('one').innerHTML = 'Hello World';
+//task 7
+document.getElementById('two').innerHTML = 12*12;
+//task 8
+document.querySelector('.one').innerHTML = 'Hello World';
+//task 9
+document.querySelector('h2>span').innerHTML = 'world';
+//task 10
+document.querySelector('.three')
+//task 11
+//task 12
+//task 13
+//task 14
+//task 15
+//task 16
